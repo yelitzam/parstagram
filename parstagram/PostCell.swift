1d7f32257da34f07e80c35390dbda9c1e472316c
yelitzam <ymendez290@gmail.com>
2020-11-15 17:25:24 -0800

    finished

---
+++ parstagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  parstagram
+//
+//  Created by Litza Mendez on 11/15/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
